c42757dc9552823982d5421cf0d74a45b7f360fb
emily-yang <cigoua@gmail.com>
2017-09-21 04:05:01 -0700

    added search function

--- src/Search.java
+++ src/Search.java
@@ -6,9 +6,11 @@ public class Search {
     // don't forget to stem
 
     private PositionalInvertedIndex index = new PositionalInvertedIndex();
+    private String query;
 
-    public Search(PositionalInvertedIndex index) {
+    public Search(PositionalInvertedIndex index, String query) {
         this.index = index;
+        this.query = query;
     }
 
     public ArrayList<Integer> mergeLists(ArrayList<Integer> listA, ArrayList<Integer> listB) {
@@ -17,14 +19,15 @@ public class Search {
 
         if (listA == null || listB == null) return null; //return null if there's an empty list
 
-        ArrayList<Integer> results = new ArrayList<Integer>();
+        List<Integer> results = new ArrayList<Integer>();
         int i = 0, j = 0;
         while (i < listA.size() && j < listB.size()) {
-            if (listA.get(i).intValue() == listB.get(j).intValue()) {
+            int compare = listA.get(i).compareTo(listB.get(j));
+            if (compare == 0) {
                 results.add(i);
                 i++;
                 j++;
-            } else if (listA.get(i).compareTo(listB.get(j)) > 0) {
+            } else if (compare > 0) {
                 j++;
             } else {
                 i++;
@@ -33,10 +36,10 @@ public class Search {
         return results;
     }
 
-    public ArrayList<Integer> getDocIDList(String term) {
+    public List<Integer> getDocIDList(String term) {
         // get list of documents that contain the given term
-        ArrayList<PositionalIndex> postings = index.getPostings(term);
-        ArrayList<Integer> docList = new ArrayList<Integer>(postingsList.size());
+        List<PositionalIndex> postings = index.getPostings(term);
+        List<Integer> docList = new ArrayList<Integer>(postingsList.size());
         for (int i = 0; i < postings.size() i++) {
             docList.set(i, postings.get(i).getDocID());
         }
@@ -46,22 +49,22 @@ public class Search {
     public ArrayList<Integer> searchPhraseLiteral(String phrase) {
         // returns a list of docIDs that contain the entire phrase
 
-        ArrayList<Integer> results = new ArrayList<Integer>();
+        List<Integer> results = new ArrayList<Integer>();
 
         // separate phrase into individual tokens
         String[] tokens = Query.getPhraseTokens(phrase);
 
         // get list of documents that contain all the phrase tokens
-        ArrayList<Integer> accum = getDocIDList(tokens[0]);
+        List<Integer> accum = getDocIDList(tokens[0]);
         for (int i = 1; i < tokens.length; i++) {
-            ArrayList<Integer> curr = getDocIDList(tokens[i]);
+            List<Integer> curr = getDocIDList(tokens[i]);
             accum = mergeLists(accum, curr);
             if (accum == null) return null;
         }
 
         // loop through each doc that contains all the tokens
-        for (Integer docID: merged) {
-            ArrayList<Integer>[] tokenPositions = new ArrayList<Integer(tokens.length);
+        for (Integer docID: accum) {
+            List<Integer>[] tokenPositions = new ArrayList<Integer(tokens.length);
             // get position list of each phrase token in the current document
             for (int i= 0; i < tokenPositions.size(); i++) {
                 tokenPositions[i] = index.getPostingByDoc(tokens[i], docID.value());
@@ -70,6 +73,7 @@ public class Search {
             // loop through first token's positions and check if other tokens are adjacent
             boolean matched = true;
             for (int i = 0; i < tokenPositions[0].size(); i++) {
+                matched = true;
                 int start = tokenPositions[0].get(i).intValue();
                 for (int j = 1; j < tokenPositions.length; j++) {
                     if (!tokenPositions[j].contains(new Integer(start + j))) {
@@ -86,11 +90,38 @@ public class Search {
         return results;
     }
 
-    public ArrayList<String> searchQuery(String query) {
-        // process each subquery one at a time
+    public ArrayList<String> searchQuery() {
+        // search the document for the query
 
-        // then combine results with OR logic
-        return null;
+        List<ArrayList<Integer>> subqueryResults = new ArrayList<ArrayList<Integer>>();
+        Set<Integer> finalResults = new HashSet<Integer>();
+
+        // process each subquery one at a time
+        String[] subqueries = Query.getSubqueries(query);
+        for (String subquery: subqueries) {
+            List<ArrayList<Integer>> literalsPostings = new ArrayList<ArrayList<Integer>>();
+            List<String> literals = Query.getQueryLiterals(subquery);
+            // get postings lists for each literal
+            for (String literal: literals) {
+                if (literal.startsWith("\"")) {  // phrases
+                    literalsPostings.add(searchPhraseLiteral(literal));
+                } else { // single tokens
+                    literalsPostings.add(index.getPostings(literal));
+                }
+            }
+            // merge postings lists for each literal
+            List<Integer> mergedList = literalsPostings.get(0);
+            for (int i = 1; i < literalsPostings.size(); i++) {
+                mergedList = mergeLists(mergedList, literalsPostings.get(i));
+            }
+            subqueryResults.add(mergedList);
+        }
+
+        // OR the results from the subqueries
+        for (ArrayList<Integer> subquery: subqueryResults) {
+            finalResults.addAll(subquery);
+        }
+        return finalResults;
     }
 
     public void printResults() {
