7f8098c95714eb76ac55292c71c3c925977caf59
Marian Nguyen <itsmariann@gmail.com>
2017-09-20 23:30:47 -0700

    added special queries (:q, :stem, :index, and :vocab)

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,5 @@
 import com.google.gson.*;
+import org.tartarus.snowball.ext.englishStemmer;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -28,17 +29,58 @@ public class Driver {
                     indexFile(file, index, i);
                     i++;
                 }
-
-            }
-            else {
+            } else {
                 System.out.println("Error: Directory invalid");
             }
-
         }
         System.out.println("end");
+
+        String query = "";
+        while (!query.equals(":q")) {
+            System.out.println("Enter search query: ");
+            query = in.nextLine();
+
+            if (query.startsWith(":stem")) {
+                // stems a term given by the user
+                englishStemmer stemmer = new englishStemmer();
+                String wordToBeStemmed = query.substring(6);    // removes prefix
+
+                stemmer.setCurrent(wordToBeStemmed);
+                stemmer.stem();
+                String wordAfterStemmed = stemmer.getCurrent();
+                System.out.println(wordToBeStemmed + " --stemmed--> " + wordAfterStemmed);  // test
+            } else if (query.startsWith(":index")) {
+                // index the folder specified (exactly like above)
+                dir = query.substring(7);
+                File f = new File(dir);
+
+                if(f.exists() && f.isDirectory()){
+                    String[] fileList = f.list();
+                    Parser parser = new Parser();
+                    int i = 1;
+                    for(String path : fileList){
+                        String[] file = parser.parseJSON(f.getPath()+"/"+path);
+                        indexFile(file, index, i);
+                        i++;
+                    }
+                } else {
+                    System.out.println("Error: Directory invalid");
+                }
+            } else if (query.startsWith(":vocab")) {
+                // prints out all terms in vocabulary
+                String[] keys = index.getDictionary();
+                for (String k : keys) {
+                    System.out.println(k);
+                }
+
+                int vocabTotal = index.getTermCount();
+                System.out.println("\nTotal number of vocabulary terms: " + vocabTotal);
+            } else {
+                // normal query ?
+            }
+        }
+
     }
-
-
 
     private static void indexFile(String[] fileData, PositionalInvertedIndex index,
                                   int docID){
