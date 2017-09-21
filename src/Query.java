ece62472eeb35eb17b757a2c6d9deb58a91b18b9
Marian Nguyen <itsmariann@gmail.com>
2017-09-20 23:30:18 -0700

    commented out some stuff that emily is still working on so I can run the project

--- src/Query.java
+++ src/Query.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 // utility methods for processing queries
 public class Query {
     // TODO deal with hyphens
--- src/Search.java
+++ src/Search.java
@@ -31,44 +31,44 @@ public class Search {
         return results;
     }
 
-    public ArrayList<Integer> getDocIDList(String term) {
-        ArrayList<PositionalIndex> postingsList = index.getPostings(term);
-        ArrayList<Integer> docList = new ArrayList<Integer>(postingsList.size());
-        int i = 0;
-        for (PositionalIndex post: postingsList) {
-            docList.set(i,post.getDocID());
-            i++;
-        }
-        return docList;
-    }
+//    public ArrayList<Integer> getDocIDList(String term) {
+//        ArrayList<PositionalIndex> postingsList = index.getPostings(term);
+//        ArrayList<Integer> docList = new ArrayList<Integer>(postingsList.size());
+//        int i = 0;
+//        for (PositionalIndex post: postingsList) {
+//            docList.set(i,post.getDocID());
+//            i++;
+//        }
+//        return docList;
+//    }
 
-    public ArrayList<Integer> searchPhraseLiteral(String phrase) {
-        ArrayList<Integer> results = new ArrayList<Integer>();
-        // separate phrase into tokens
-        String[] tokens = Query.getPhraseTokens(phrase);
-
-        // cycle through each token and merge postings lists
-        ArrayList<Integer> merged = getDocIDList(tokens[0]);
-        for (int i = 1; i < tokens.length; i++) {
-            ArrayList<Integer> curr = getDocIDList(tokens[i]);
-            merged = mergeLists(merged, curr);
-        }
-
-        // check positions in each doc that contains all the tokens
-
-        // loop through each doc that matched
-        // loop through tokens[0]'s positions in the doc
-        // loop through the other tokens and see if they're adjacent..
-
-        for (Integer docID: merged) {
-            for (Integer position: index.getPostingsByDoc(tokens[0], docID.intValue())) {
-
-            }
-        }
-
-
-        return null;
-    }
+//    public ArrayList<Integer> searchPhraseLiteral(String phrase) {
+//        ArrayList<Integer> results = new ArrayList<Integer>();
+//        // separate phrase into tokens
+//        String[] tokens = Query.getPhraseTokens(phrase);
+//
+//        // cycle through each token and merge postings lists
+//        ArrayList<Integer> merged = getDocIDList(tokens[0]);
+//        for (int i = 1; i < tokens.length; i++) {
+//            ArrayList<Integer> curr = getDocIDList(tokens[i]);
+//            merged = mergeLists(merged, curr);
+//        }
+//
+//        // check positions in each doc that contains all the tokens
+//
+//        // loop through each doc that matched
+//        // loop through tokens[0]'s positions in the doc
+//        // loop through the other tokens and see if they're adjacent..
+//
+//        for (Integer docID: merged) {
+//            for (Integer position: index.getPostingsByDoc(tokens[0], docID.intValue())) {
+//
+//            }
+//        }
+//
+//
+//        return null;
+//    }
 
     public ArrayList<String> searchQuery(String query) {
         // process each subquery one at a time
