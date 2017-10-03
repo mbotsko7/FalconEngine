b72ebc7c99ba0ba4862a2fc6f1031414a945fd72
Marian Nguyen <itsmariann@gmail.com>
2017-10-03 00:32:03 -0700

    finishing up unit testing

--- Test/KGramIndexTest.java
+++ Test/KGramIndexTest.java
@@ -1,5 +1,26 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class KGramIndexTest {
+    private KGramIndex k = new KGramIndex();
+
+    @Test
+    public void add() throws Exception {
+        String s = "marian";
+        k.add(s);
+        String[] kgrams = {"m", "a", "r", "i", "a", "n", "$m", "ma", "ar", "ri", "ia",
+                            "an", "n$", "$ma", "mar", "ari", "ria", "ian", "an$"};
+        for (int i = 0; i < kgrams.length; i++) {
+            assertTrue(k.find(kgrams[i]).contains(s));
+        }
+
+        // $marian$
+        // m, a, r, i, a, n
+        // $m, ma, ar, ri, ia, an, n$
+        // $ma, mar, ari, ria, ian, an$
+    }
 
 }
