f84eb4a837608abb06ef40a77b610db91d3bc4e9
mbotsko7 <mbotsko7@gmail.com>
2017-11-07 12:42:29 -0800

    ranking, a beginning

--- src/RankedRetrieval.java
+++ src/RankedRetrieval.java
@@ -1,20 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class RankedRetrieval {
+    protected ArrayList<String> query;
+    private DiskInvertedIndex diskInvertedIndex;
+    private ArrayList<DocumentWeight> documentWeights;
 
-    public RankedRetrieval(){
-
+    public RankedRetrieval(ArrayList<String> q, DiskInvertedIndex index, ArrayList<DocumentWeight> docWeights){
+        query = q;
+        diskInvertedIndex = index;
+        documentWeights = docWeights;
     }
 
     public double rank(){
-        /*
-        calculate Wqt = ln(1+ (number of docs/docs containing the term)
-        for each doc in term posting list:
-            accumulator shenanigans
-            fetch Wdt
-            Ad += Wdt X Wqt
-        for every non-zero Ad
-            Ad /= Ld //Ld in docWeights.bin
-        PQ values return top ten
-         */
+        //calculate Wqt = ln(1+ (number of docs/docs containing the term)
+        //for each doc in term posting list
+        for(String token : query){
+            DiskPosting[] postings = diskInvertedIndex.getPostings(token);
+            for(DiskPosting diskPosting : postings){
+                //accumlate the value
+                //fetch Wdt
+                //Ad += Wdt X Wqt
+            }
+            //for every non-zero Ad
+                //Ad /= Ld of docWeights.bin
+                //PQ add Ad
+            //return top ten
+        }
         return 0.0;
     }
 
