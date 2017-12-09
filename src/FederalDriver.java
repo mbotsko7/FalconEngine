f5a60692960125a6ebefc43d362ab4c5ebd440c7
mbotsko7 <mbotsko7@gmail.com>
2017-12-09 09:59:38 -0800

    knn improvements

--- src/FederalDriver.java
+++ src/FederalDriver.java
@@ -91,12 +91,14 @@ public class FederalDriver {
         }
 
         System.out.println("++ FINISH PROGRAM");
+
+        kNNClassifier classify = new kNNClassifier(uIndex, new FederalistIndex[]{hIndex, mIndex, jIndex});
     }
 
     // used to index entire directory
     private static void indexDirectory(FederalistIndex index, String folderName) {
         System.out.println("starting " + folderName + " indexing...");
-        File f = new File("/home/bardsko/FederalistPapers/" + folderName);
+        File f = new File("FederalistPapers/" + folderName);
         if (f.exists() && f.isDirectory()) {
             String[] fileList = f.list();
             Arrays.sort(fileList, new FileComparator());    // sorts files before assigning docID
--- src/kNNClassifier.java
+++ src/kNNClassifier.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class kNNClassifier {
     private FederalistIndex unclassified;
     private FederalistIndex[] indexArr;
-    private String[] terms;
+    private ArrayList<String> terms;
 
     /*
     TO DO:
@@ -15,10 +16,28 @@ public class kNNClassifier {
     public kNNClassifier(FederalistIndex unclassified, FederalistIndex[] indexArr){
         this.unclassified = unclassified;
         this.indexArr = indexArr;
-        terms = unclassified.getDictionary();
+        terms = new ArrayList<>();
+        for(String s : unclassified.getDictionary())
+            terms.add(s);
+        for(FederalistIndex f : indexArr){
+            for(String s : f.getDictionary())
+                terms.add(s);
+        }
+        Collections.sort(terms);
+        String prev = terms.get(0);
+        for(int i = 1; i < terms.size(); i++){
+            if(terms.get(i).equals(prev)){
+                terms.remove(i);
+                i--;
+            }
+            else
+                prev = terms.get(i);
+        }
         process();
     }
 
+
+    //CONSIDER ALL THE VOCABULARY
     public void process(){
         //for each string in the unclassified document
         double min = Double.MAX_VALUE;
@@ -28,11 +47,34 @@ public class kNNClassifier {
 //        madison.add(getVector(indexArr[1]));
 //        jay.add(getVector(indexArr[2]));
 //        unknown.add(getVector(unclassified));
-        int h = 0, m = 0, j2 = 0;
+        int h = 0, m = 0, j2 = 0, a = 0, b = 0, c = 0;
+        for(ArrayList<Double> vector : unknown){
+            int n = 3;
+            double[] ham = getClosest(vector, hamilton,n);
+            double[] mad = getClosest(vector, madison, n);
+            double[] j = getClosest(vector, jay, n);
+            for(Double d : ham) System.out.print(d+" ");
+            System.out.println();
+            for(Double d : mad) System.out.print(d+" ");
+            System.out.println();
+            switch (whodunnit2(ham, mad, j, n)){
+                case "Hamilton":
+                    h++;
+                    break;
+                case "Madison":
+                    m++;
+                    break;
+                case "Jay":
+                    j2++;
+                    break;
+            }
+        }
+        /*
         for(ArrayList<Double> vector : unknown){
             double ham = getAverageClassDistance(vector, hamilton, 5);
             double mad = getAverageClassDistance(vector, madison, 5);
             double j =  getAverageClassDistance(vector, jay, 5);
+            System.out.println("h "+ham+"\nm "+mad+"\nj "+j);
             switch (whodunnit(ham, mad, j)){
                 case "Hamilton":
                     h++;
@@ -45,9 +87,86 @@ public class kNNClassifier {
                     break;
             }
         }
+        */
         System.out.println("Hamilton: "+h);
         System.out.println("Jay: "+j2);
-        System.out.println("Madison"+m);
+        System.out.println("Madison: "+m);
+    }
+
+
+
+    public String whodunnit2(double[] ham, double[] mad, double[] jay, int n){
+        int[] count = new int[3];
+        int a = 0, b = 0, c = 0;
+        while(n > 0){
+            switch (whodunnit(ham[a], mad[b], jay[c])){
+                case "Hamilton":
+                    a++;
+                    count[0]++;
+                    break;
+                case "Madison":
+                    b++;
+                    count[1]++;
+                    break;
+                case "Jay":
+                    c++;
+                    count[2]++;
+                    break;
+            }
+            n--;
+        }
+        for(Integer i : count) System.out.print(i+" ");
+        System.out.println();
+        return whodunnit3(count[0], count[1], count[2]);
+    }
+
+    public String whodunnit3(double ham, double mad, double jay){
+        if(ham > mad && ham > jay)
+            return "Hamilton";
+        else if(mad > ham && mad > jay)
+            return "Madison";
+        else
+            return "Jay";
+    }
+
+    public String whodunnit(double ham, double mad, double jay){
+        if(ham < mad && ham < jay)
+            return "Hamilton";
+        else if(mad < ham && mad < jay)
+            return "Madison";
+        else
+            return "Jay";
+    }
+
+    //given a list of classified document vectors, calculate the distance of each
+    //return average distance of the closest n
+    public double[] getClosest(ArrayList<Double> vector, ArrayList<ArrayList<Double>> classVectors, int n){
+        PriorityQueue<Double> priorityQueue = new PriorityQueue<>();
+        for(ArrayList<Double> list : classVectors){
+            priorityQueue.add(distance(vector, list));
+        }
+        double[] arr = new double[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = priorityQueue.poll();
+        }
+
+        return arr;
+    }
+
+    //given a list of classified document vectors, calculate the distance of each
+    //return average distance of the closest n
+    public double getAverageClassDistance(ArrayList<Double> vector, ArrayList<ArrayList<Double>> classVectors, int n){
+        PriorityQueue<Double> priorityQueue = new PriorityQueue<>();
+        for(ArrayList<Double> list : classVectors){
+            priorityQueue.add(distance(vector, list));
+        }
+        double avg = 0;
+        for(int i = 0; i < n; i++){
+            avg += priorityQueue.poll();
+        }
+
+        avg /= n;
+        return avg;
     }
 
     public ArrayList<ArrayList<Double>> getVector(FederalistIndex federalistIndex){
@@ -63,36 +182,21 @@ public class kNNClassifier {
                 else
                     weights.add(1.0);
             }
+            double len =  1;//length(weights);
+            for(int i = 0; i < weights.size(); i++){
+                weights.set(i, weights.get(i)/len);
+            }
             list.add(weights);
         }
         return list;
     }
 
-    public String whodunnit(double ham, double mad, double jay){
-        if(ham < mad && ham < jay)
-            return "Hamilton";
-        else if(mad < ham && mad < jay)
-            return "Madison";
-        else
-            return "Jay";
+    public double length(ArrayList<Double> vector){
+        double total = 0;
+        for(Double d : vector)
+            total += Math.pow(d,2);
+        return Math.sqrt(total);
     }
-
-    //given a list of classified document vectors, calculate the distance of each
-    //return average distance of the closest n
-    public double getAverageClassDistance(ArrayList<Double> vector, ArrayList<ArrayList<Double>> classVectors, int n){
-        PriorityQueue<Double> priorityQueue = new PriorityQueue<>();
-        for(ArrayList<Double> list : classVectors){
-            priorityQueue.add(distance(vector, list));
-        }
-        double avg = 0;
-        for(int i = 0; i < 10; i++){
-            avg += priorityQueue.poll();
-        }
-
-        avg /= n;
-        return avg;
-    }
-
     //calculate euclidean distance
     public double distance(ArrayList<Double> vector1, ArrayList<Double> vector2){
         double total = 0;
